a08e1d555900e4c39b160dd40f525a0ef5fc10fb
kajalsharnagat <153443569+kajalsharnagat@users.noreply.github.com>
2023-12-21 16:51:17 +0530

    Delete csv_upload directory

--- csv_upload/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import upload_csv, download_csv
-
-urlpatterns = [
-    path('upload-csv/', upload_csv, name='upload_csv'),
-    path('download-csv/', download_csv, name='download_csv')
-    # url 2
-    # url 3
-]
